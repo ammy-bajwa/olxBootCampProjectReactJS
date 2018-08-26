dddf1ebc4ec31f8e47a8a8d1d3b62fbf2c6552ed
AMIR SADIQ ALI <amirsadiqalisaifi@gmail.com>
2018-08-26 23:50:55 +0500

    changing name for index.html js file

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/app.js',
     output: {
         path: path.join(__dirname,'public'),
-        filename: 'bundle.js'
+        filename: 'app.js'
     },
     module: {
         rules: [{
