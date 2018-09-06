f7d22ca33cb4fd06571cb3c3b48378d24c196ea6
ammy203 <33599273+ammy203@users.noreply.github.com>
2018-09-06 14:14:39 +0500

    adding https support

--- public/index.js
+++ public/index.js
@@ -1,15 +1,14 @@
-
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDX8myqZhSBYg-DH13Cy4moZSnSh8Hxs6k",
-    authDomain: "olx-clone-app.firebaseapp.com",
-    databaseURL: "https://olx-clone-app.firebaseio.com",
-    projectId: "olx-clone-app",
-    storageBucket: "olx-clone-app.appspot.com",
-    messagingSenderId: "318699316380"
-  };
-  firebase.initializeApp(config);
-  // Retrieve Firebase Messaging object.
+  apiKey: "AIzaSyDX8myqZhSBYg-DH13Cy4moZSnSh8Hxs6k",
+  authDomain: "olx-clone-app.firebaseapp.com",
+  databaseURL: "https://olx-clone-app.firebaseio.com",
+  projectId: "olx-clone-app",
+  storageBucket: "olx-clone-app.appspot.com",
+  messagingSenderId: "318699316380"
+};
+firebase.initializeApp(config);
+// Retrieve Firebase Messaging object.
 var messaging = firebase.messaging();
 // if ('serviceWorker' in navigator) {
 //     navigator.serviceWorker.register('service-worker.js');
@@ -17,30 +16,35 @@ var messaging = firebase.messaging();
 //     // });
 // }
 messaging.onMessage(function(payload) {
-  console.log('Message received. ', payload);
+  console.log("Message received. ", payload);
 });
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js').then((registration) => {
+if ("serviceWorker" in navigator && location.protocol === 'https:') {
+  window.addEventListener("load", function(event) {
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then(function(registration) {
         // Successfully registers service worker
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
         messaging.useServiceWorker(registration);
       })
-      .then(() => {
+      .then(function() {
         // Requests user browser permission
         return messaging.requestPermission();
       })
-      .then(() => {
+      .then(function() {
         // Gets token
         return messaging.getToken();
       })
-      .then((token) => {
+      .then(function(token) {
         // Simple ajax call to send user token to server for saving
-        localStorage.setItem('token',token)
-        console.log('index.js ',token)
+        localStorage.setItem("token", token);
+        console.log("index.js ", token);
       })
-      .catch((err) => {
-        console.log('ServiceWorker registration failed: ', err);
+      .catch(function(err) {
+        console.log("ServiceWorker registration failed: ", err);
       });
-    });
-    }
+  });
+}
