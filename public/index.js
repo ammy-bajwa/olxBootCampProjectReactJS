5cc25e098063af9ac187372c5327d163c71c15f2
ammy203 <33599273+ammy203@users.noreply.github.com>
2018-09-12 17:26:20 +0500

    solving add to home prompt issue

--- public/index.js
+++ public/index.js
@@ -19,6 +19,36 @@ messaging.onMessage(function(payload) {
   console.log("Message received. ", payload);
 });
 if ("serviceWorker" in navigator) {
+  var deferredPrompt;
+
+  window.addEventListener("beforeinstallprompt", function(e) {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+
+    showAddToHomeScreen();
+  });
+  function addToHomeScreen() {
+    var a2hsBtn = document.querySelector(".ad2hs-prompt");
+
+    // hide our user interface that shows our A2HS button
+    a2hsBtn.style.display = "none";
+
+    // Show the prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+
+      deferredPrompt = null;
+    });
+  }
   window.addEventListener("load", function(event) {
     navigator.serviceWorker
       .register("service-worker.js")
@@ -28,42 +58,6 @@ if ("serviceWorker" in navigator) {
           "ServiceWorker registration successful with scope: ",
           registration.scope
         );
-       var deferredPrompt;
-
-    window.addEventListener(' ', function (e) {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-
-      showAddToHomeScreen();
-
-    });
-    function addToHomeScreen() {
-      var a2hsBtn = document.querySelector(".ad2hs-prompt");
-
-      // hide our user interface that shows our A2HS button
-      a2hsBtn.style.display = 'none';
-
-      // Show the prompt
-      deferredPrompt.prompt();
-
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice
-        .then(function (choiceResult) {
-
-
-
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-
-          deferredPrompt = null;
-
-        });
-    }
         messaging.useServiceWorker(registration);
       })
       .then(function() {
